e4f755b4419cf554142e47d5114f8e96779e3ae6
ikeyoshy <ikeyoshi1@gmail.com>
2017-05-18 10:19:00 +0900

    Modify pictures

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'awsiot-handson-basic'
+project = u'awsiot-handson-dojo-basic'
 copyright = u'2017, ikeyoshy'
 
 # The version info for the project you're documenting, acts as replacement for
